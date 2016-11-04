9bdbaea41b25964f2f44c674bb60de8973576077
noelis <soto.noelis+vagrant@gmail.com>
2016-11-04 23:46:52 +0000

    Refactored code and seeded db.

--- seed.py
+++ seed.py
@@ -9,89 +9,105 @@ from sqlalchemy.orm.exc import NoResultFound
 import xmltodict
 import requests
 
+def load_book_info():
+    """Load information to book, author and book_authors tables in db."""
 
-def get_book_titles(api_url):
-    """Get title, year published and mini-summary from API into a dictionary. """
+    for i in range(1, 6):
+        print "Loading books.."
+        books = book_api_request('http://www.feedbooks.com/books/top.atom?page=' + str(i))
 
-    print "Loading books.."
+        for book in books:
+            title, release_year, summary, download_url, book_cover = get_book_metadata(book)
+            book_added = add_book_metadata(title, release_year, summary, download_url, book_cover)
+            get_author(book, book_added)
 
-    #Response object from API
+def book_api_request(api_url):
+    """Make API request, get a response in text format, parse it using xmltodict."""
+
     response = requests.get(api_url)
 
     # Turn response.content (string) into dictionary
     metadata = xmltodict.parse(response.content)
 
+    # List of 'entry' objects from 'feed'dictionary. Each 'entry' is a book (dict).
     books = metadata['feed']['entry']
 
-    # List of book 'entry' objects from nested dictionary.
-    # Each book object is a dictionary.
-    books = metadata['feed']['entry']
+    return books
 
-    for book in books:
-        title = book['title']
-        release_year = book['dcterms:issued']
-        summary = book['summary'].encode("utf-8")
-        download_url = book['link'][1]['@href']
-        book_cover = book['link'][4]['@href']
+def get_book_metadata(book):
+    """ Gets book metadata from API."""
 
-        # Creates an instance of the Book class
-        book_add = Book(title=title, 
-                    release_year=release_year,
-                    url=download_url,
-                    book_cover=book_cover,
-                    mini_summary=summary)
+    title = book['title']
+    release_year = book['dcterms:issued']
+    summary = book['summary'].encode("utf-8")
+    download_url = book['link'][1]['@href']
+    book_cover = book['link'][4]['@href']
+    return [title, release_year, summary, download_url, book_cover]
 
-        # Adds book to the session.
-        db.session.add(book_add)
 
-        # Commits book so it gets added to the table.
+def add_book_metadata(title, release_year, summary, download_url, book_cover):
+    """Creates Book object and adds metadata to db."""
+
+    # Creates an instance of the Book class
+    book_add = Book(title=title, 
+                release_year=release_year,
+                url=download_url,
+                book_cover=book_cover,
+                mini_summary=summary)
+
+    # Add/commit book so it gets added to the table.
+    db.session.add(book_add)
+    db.session.commit()
+
+    return book_add
+
+def get_author(book, book_add):
+    """ Gets author info from book."""
+
+    if isinstance(book['author'], dict):
+        author_name = book['author']['name'].encode('utf-8')
+        author_added = add_author(author_name)
+        link_author_book(author_added, book_add)
+        
+    else:
+        # List of dictionaries
+        book_authors = book['author'] 
+
+        for author in book_authors:
+            # Grabs author from dict obj
+            author_name = author['name'].encode('utf-8') 
+            author_added = add_author(author_name)
+            link_author_book(author_added, book_add)
+
+def add_author(author_name):
+    """ Creates Author object, check if it already exists in db & adds it."""
+
+    # Creates an instance of the Author class
+    author = Author(name=author_name)
+
+    try:
+        # Check if author exists & do not add them if so.
+        check_author = Author.query.filter(Author.name == author_name).one()
+    except NoResultFound:
+
+        # If we get this error, then author doesn't exist yet. Add/commit to db
+        db.session.add(author)
         db.session.commit()
 
-        # Check if book['author'] is a dictionary object
-        if isinstance(book['author'], dict):
-            author_name = book['author']['name'].encode('utf-8')
-            author = Author(name=author_name) # Creates an instance of the Author class
-            try:
-                # Check in the database to see if this author exists. 
-                # If we get 1 result, then don't add them to the table. 
-                get_authors = Author.query.filter(Author.name == author_name).one()
-                pass
-            except NoResultFound:
-                # If we check in the db and get this error, then author doesn't exist yet.
-                #  Add/commit to db
-                db.session.add(author)
-                db.session.commit()
+    check_author = Author.query.filter(Author.name == author_name).one()
+    return check_author
 
-            # Query the Author table by name to find the author_id
-            get_author = Author.query.filter(Author.name == author_name).one() # Returns an author object
 
-            # Creates BookAuthor instance 
-            book_author_link = BookAuthor(book_id=book_add.book_id, 
-                                            author_id=get_author.author_id)
+def link_author_book(author_added, book_add):
+    """Creates a BookAuthor object and adds it to db."""
 
-            # Add/commit instance to BookAuthor association table.
-            db.session.add(book_author_link)
-            db.session.commit()
-        else:
-            book_authors = book['author'] # List of dictionaries
+     # Creates an instance of the BookAuthor class
+    book_author_link = BookAuthor(book_id=book_add.book_id, 
+                                    author_id=author_added.author_id)
 
-            for author in book_authors:
-                author_name = author['name'].encode('utf-8') # Grabs author from dict obj
-
-                author_pair = Author(name=author_name) # Creates an instance of the Author class
-                
-                db.session.add(author_pair) # Add/commit author_pair instance
-                db.session.commit()
-                
-                # Query the Author table by name to find the author_id
-                get_authors = Author.query.filter(Author.name == author_name).one()
-
-                # Creates an instance of the BookAuthor class
-                book_author_link = BookAuthor(book_id=book_add.book_id, 
-                                            author_id=get_authors.author_id)
-                 # Add/commit author_pair instance
-                db.session.add(book_author_link)
-                db.session.commit()
+    # Add/commit instance to BookAuthor association table.
+    db.session.add(book_author_link)
+    db.session.commit()
 
                 
 if __name__ == '__main__':
@@ -101,10 +117,6 @@ if __name__ == '__main__':
     # Create tables
     db.create_all()
 
-    # Call functions to load info from each API endpoint page
-    get_book_titles('http://www.feedbooks.com/books/top.atom')
-    get_book_titles('http://www.feedbooks.com/books/top.atom?page=2')
-    get_book_titles('http://www.feedbooks.com/books/top.atom?page=3')
-    get_book_titles('http://www.feedbooks.com/books/top.atom?page=4')
-    get_book_titles('http://www.feedbooks.com/books/top.atom?page=5')
+    # Call function to load books to db.
+    load_book_info()
 
